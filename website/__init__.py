aedcc3a572019141ecd2a4afe0bed599a737deff
Samuel <sjn32@case.edu>
2023-02-21 14:52:15 -0600

    env variables

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -9,6 +10,14 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    # Client info
+    CLIENT_ID = os.getenv('CLIENT_ID')
+    CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+    # Spotify API endpoints
+    TOKEN_URL = 'https://accounts.spotify.com/api/token'
+    TOP_ENDPOINT = 'https://api.spotify.com/v1/me/top/tracks'
     
 
     return app
